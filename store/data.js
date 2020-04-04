cd82e9374c798dcb67b68c9bdca9f2a8e4a9e775
ikeda kohei <tdn.suama@gmail.com>
2020-04-05 08:33:28 +0900

    取得データのコピー方法変更

--- store/data.js
+++ store/data.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import cloneDeep from 'lodash/cloneDeep'
 import Data from '@/data/data.json'
 
 export const state = () => ({
@@ -17,10 +18,7 @@ export const actions = {
       data: Data
     }
     await axios.get(process.env.dataUrl).then(res => {
-      payload.data.contacts = res.data.contacts
-      payload.data.inspections = res.data.inspections
-      payload.data.inspections_summary = res.data.inspections_summary
-      payload.data.lastUpdate = res.data.lastUpdate
+      payload.data = cloneDeep(res.data)
     })
     context.commit('getData', payload)
   }
