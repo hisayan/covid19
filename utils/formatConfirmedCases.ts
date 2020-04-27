c1143b3a0b7d55507c0a1f9d75ef00f68d16f803
Kohei Ikeda <tdn.suama@gmail.com>
2020-04-27 16:26:48 +0900

    fix lint error

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -39,7 +39,7 @@ type ConfirmedCasesType = {
 export default (data: DataType) => {
   const formattedData: ConfirmedCasesType = {
     検査実施人数: data.value,
-    陽性物数: data.children.reduce((sum,child) => sum + child.value, 0),
+    陽性物数: data.children.reduce((sum, child) => sum + child.value, 0),
     入院中: data.children[0].value,
     死亡: data.children[3].value,
     退院: data.children[2].value
