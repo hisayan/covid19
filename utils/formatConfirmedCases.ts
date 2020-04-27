b31c5e7261726cfdf3175f0e00ab0f97b6420c2e
Kohei Ikeda <tdn.suama@gmail.com>
2020-04-27 16:20:09 +0900

    検査陽性者の状況のデータ整形のエラー修正

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -39,10 +39,10 @@ type ConfirmedCasesType = {
 export default (data: DataType) => {
   const formattedData: ConfirmedCasesType = {
     検査実施人数: data.value,
-    陽性物数: data.children[0].value,
-    入院中: data.children[0].children[0].value,
-    死亡: data.children[0].children[2].value,
-    退院: data.children[0].children[1].value
+    陽性物数: data.children.reduce((sum,child) => sum + child.value, 0),
+    入院中: data.children[0].value,
+    死亡: data.children[3].value,
+    退院: data.children[2].value
   }
   return formattedData
 }
