6b98688131ba74dde5d4a9d0576629ff82e7e877
kohei ikeda <tdn.suama@gmail.com>
2020-11-12 16:19:44 +0900

    表示する陽性者数を統一

--- components/cards/ConfirmedCasesAttributesCard.vue
+++ components/cards/ConfirmedCasesAttributesCard.vue
@@ -13,8 +13,9 @@
 </template>
 
 <script>
+import dayjs from 'dayjs'
+
 import Data from '@/data/data.json'
-import formatGraph from '@/utils/formatGraph'
 import formatTable from '@/utils/formatTable'
 import DataTable from '@/components/DataTable.vue'
 
@@ -23,17 +24,14 @@ export default {
     DataTable
   },
   data() {
-    // 感染者数グラフ
-    const patientsGraph = formatGraph(Data.patients_summary.data)
     // 感染者数
     const patientsTable = formatTable(Data.patients.data)
+    const dateAsOf = dayjs(Data.patients.date).format('MM/DD')
 
     const sumInfoOfPatients = {
-      lText: patientsGraph[
-        patientsGraph.length - 1
-      ].cumulative.toLocaleString(),
+      lText: patientsTable.datasets.length.toLocaleString(),
       sText: this.$t('{date}の累計', {
-        date: patientsGraph[patientsGraph.length - 1].label
+        date: dateAsOf
       }),
       unit: this.$t('人')
     }
