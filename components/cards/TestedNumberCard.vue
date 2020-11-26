8fa6f84e1b605a5337c8f91e017ab987427a2029
kohei ikeda <tdn.suama@gmail.com>
2020-11-27 01:31:18 +0900

    グラフの表示方法改善 (#173)
    * WIP:グラフのスクロール追加
    
    * Done:グラフのスクロール追加

--- components/cards/TestedNumberCard.vue
+++ components/cards/TestedNumberCard.vue
@@ -1,26 +1,31 @@
 <template>
   <v-col cols="12" md="6" class="DataCard">
-    <time-stacked-bar-chart
-      :title="$t('検査実施数')"
-      :title-id="'number-of-tested'"
-      :chart-id="'time-stacked-bar-chart-inspections'"
-      :chart-data="inspectionsGraph"
-      :date="Data.inspections_summary.date"
-      :items="inspectionsItems"
-      :labels="inspectionsLabels"
-      :unit="$t('件.tested')"
-      :url="'https://www.pref.nagano.lg.jp/joho/kensei/tokei/johoka/opendata/'"
-    />
-    <!-- 件.tested = 検査数 -->
+    <client-only>
+      <time-stacked-bar-chart
+        :title="$t('検査実施数')"
+        :title-id="'number-of-tested'"
+        :chart-id="'time-stacked-bar-chart-inspections'"
+        :chart-data="inspectionsGraph"
+        :date="Data.inspections_summary.date"
+        :items="inspectionsItems"
+        :labels="inspectionsLabels"
+        :unit="$t('件.tested')"
+        :url="
+          'https://www.pref.nagano.lg.jp/joho/kensei/tokei/johoka/opendata/'
+        "
+        :data-labels="inspectionsDataItems"
+        :table-labels="inspectionsItems"
+      />
+      <!-- 件.tested = 検査数 -->
+    </client-only>
   </v-col>
 </template>
 
 <script>
-import dayjs from 'dayjs'
-
 import Data from '@/data/data.json'
 import testCount from '@/data/test_count.json'
 import TimeStackedBarChart from '@/components/TimeStackedBarChart.vue'
+import { getDayjsObject } from '@/utils/formatDate'
 
 export default {
   components: {
@@ -28,19 +33,20 @@ export default {
   },
   data() {
     // 検査実施日別状況
-    const inspectionsGraph = [
-      testCount.map(data => data.positiveNum),
-      testCount.map(data => data.negativeNum)
-    ]
+    const positive = testCount.map(data => data.positiveNum)
+    const negative = testCount.map(data => data.negativeNum)
+    const inspectionsGraph = [positive, negative]
     const inspectionsItems = [this.$t('陽性'), this.$t('陰性')]
+    const inspectionsDataItems = [this.$t('陽性'), this.$t('陰性')]
     const inspectionsLabels = testCount.map(data => {
-      return dayjs(data.YMD).format('MM/DD')
+      return getDayjsObject(data.YMD).format('YYYY-MM-DD')
     })
 
     const data = {
       Data,
       inspectionsGraph,
       inspectionsItems,
+      inspectionsDataItems,
       inspectionsLabels
     }
     return data
